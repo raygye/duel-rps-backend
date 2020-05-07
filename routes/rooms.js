9a5dddb6fb1e760c1d33c21520767c3cd915c7a0
yeG10 <garyye2007@gmail.com>
2020-05-06 23:41:44 -0400

    game result response

--- routes/rooms.js
+++ routes/rooms.js
@@ -4,28 +4,18 @@ let User = require('../models/userSchema');
 let populations = {};
 
 router.route('/:user').post((req, res) => {
-    let user = req.params.user;
-    if (populations.user=== undefined || populations.user <=1) {
-        User.findOne({username: req.body.username}, (err, result) => {
-            if (err) throw err;
-            else {
-                res.json({
-                    'img': result.img,
-                    'wins': result.wins,
-                    'games': result.games
-                })
-            }
-        });
-    }
-    if (populations.user === undefined || populations.user === 0) {
-        populations[user] = 1;
-    }
+    User.findOne({username: req.body.username}, (err, result) => {
+        if (err) {
+            throw err;
+        }
+        else {
+            res.json({
+                'img': result.img,
+                'wins': result.wins,
+                'games': result.games
+            })
+        }
+    });
 
-    else if (populations[user] === 1) {
-        populations[user]++;
-    }
-    else {
-        res.json('Room is full.');
-    }
 });
 module.exports = router;
